5019728b1e707c633be7a52370d403cdce42fc3e
Tomas <tomush4@gmail.com>
2023-07-09 15:55:26 +0100

    Add state for grouped words

---
+++ WordFrequencyGUI/AppState.cs
@@ -0,0 +1,14 @@
+﻿namespace WordFrequencyGUI
+{
+    public class AppState
+    {
+        public List<WordFrequency>? GroupedWords {get;set;}
+    }
+
+    public class WordFrequency
+    {
+        public string? Word { get; set; }
+        public int Frequency { get; set; }
+        public string? NewsOutlet { get; set; }
+    }
+}
