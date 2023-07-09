d1c6f03c5a4e077352f063f8b0f5e56e6a52f76e
Tomas <tomush4@gmail.com>
2023-07-09 14:40:33 +0100

    Add bootstrap grid

--- WordFrequencyGUI/Program.cs
+++ WordFrequencyGUI/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WordFrequencyGUI;
-using static System.Net.WebRequestMethods;
+using BlazorBootstrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 string baseUrl = "https://wordfrequencyapi20230707193303.azurewebsites.net/";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl)});
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
