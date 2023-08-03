681eff3cd6382f0f42e5bb1a7f6879f305fad55b
Tomas <tomush4@gmail.com>
2023-08-03 08:04:54 +0100

    Add sample data, no longer call api to fetch the data.

--- WordFrequencyGUI/Program.cs
+++ WordFrequencyGUI/Program.cs
@@ -7,8 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string baseUrl = "https://wordfrequencyapi20230707193303.azurewebsites.net/";
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl)});
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
