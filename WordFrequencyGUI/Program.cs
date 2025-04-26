5b8ab71f787caff7aa4d2e00f123d55e2ccf9687
Tomas <you@example.com>
2025-04-26 21:51:34 +0100

    upgrade to .NET 8 and fetch data from API

--- WordFrequencyGUI/Program.cs
+++ WordFrequencyGUI/Program.cs
@@ -8,7 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string baseUrl = "https://localhost:7272/";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 builder.Services.AddBlazorBootstrap();
 
